02ee702201b577c63add38bb283a77b175a054da
Lodhbrok <kenanincikbusiness@gmail.com>
2022-11-24 20:51:09 +0300

    Adding exit button to appbar

--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:ledbim_project/Pages/login_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,6 +18,17 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("HomePage"),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.white,
+            ),
+            onPressed: (){
+              Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => LoginDemo()));
+            })
+        ],
       ),
       
       body: Column(
--- lib/Pages/todo_page.dart
+++ lib/Pages/todo_page.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:ledbim_project/Pages/login_screen.dart';
 
 class ToDoPage extends StatefulWidget {
   const ToDoPage({Key? key}) : super(key: key);
@@ -13,8 +16,19 @@ class _ToDoPageState extends State<ToDoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
         title: const Text("ToDoPage"),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Colors.white,
+            ),
+            onPressed: (){
+              Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => LoginDemo()));
+            })
+        ],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
