a7b8d800d637bf54dd61275a7a9aaa1ec0e210b7
Lodhbrok <kenanincikbusiness@gmail.com>
2022-11-24 19:31:36 +0300

    Creating Project and Basic BottomNavBar

---
+++ android/app/src/main/kotlin/com/example/ledbim_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ledbim_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class ToDoPage extends StatefulWidget {
+  const ToDoPage({Key? key}) : super(key: key);
+
+  @override
+  State<ToDoPage> createState() => _ToDoPageState();
+}
+
+class _ToDoPageState extends State<ToDoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
